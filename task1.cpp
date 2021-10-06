6973959bf6e6ce689cf800f8d8c2407d2434f004
AmanBhatt2401 <79921956+AmanBhatt2401@users.noreply.github.com>
2021-10-06 20:51:54 +0530

    Create task1.cpp

---
+++ task1.cpp
@@ -0,0 +1,19 @@
+//add string(a) with a integer(b) 
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a=10,b=12;
+  float a1=10.2,b1=12.2;
+  string a2="hello",b2="there";
+  string a2="hello",b2="10";
+  
+  
+  cout<<"The sum of integer is"<<(a+b)<<endl; 
+  cout<<"The sum of float is"<<(a1+b1)<<endl;
+  cout<<"The sum of string is"<<(a2+b2)<<endl;
+  cout<<"The sum of string and integer is"<<(a2+b)<<endl;
+  
+  return 0;
+  }
+     
