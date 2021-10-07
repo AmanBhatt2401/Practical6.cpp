ef30a91823a1220bb95a0fd0dab49a6363aa3308
AmanBhatt2401 <79921956+AmanBhatt2401@users.noreply.github.com>
2021-10-07 10:44:09 +0530

    Create task6.cpp

---
+++ task6.cpp
@@ -0,0 +1,14 @@
+//to check if we remove a char from string and what output it gives
+#include<iostream>
+#include<string>
+using namespace std;
+ 
+ int main()
+{
+ string s="Hello";
+ s[0]='J';
+ cout<<s;
+ 
+
+ return 0;
+}
