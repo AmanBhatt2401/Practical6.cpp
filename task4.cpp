628efa4fde711c86f6f2ebdc6c7a148cfca4e1c2
AmanBhatt2401 <79921956+AmanBhatt2401@users.noreply.github.com>
2021-10-07 21:34:06 +0530

    Create task4.cpp

---
+++ task4.cpp
@@ -0,0 +1,16 @@
+//Difference between the ASCII value of characters
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+
+char a , b;
+
+cout<<"ENTER THE CHAR A :"<<endl;
+cin>>a;
+cout<<"ENTER THE CHAR B :"<<endl;
+cin>>b;
+cout<<int(a-b);
+return 0;
+}
